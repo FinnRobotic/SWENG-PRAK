b118af19880a03ed2e5bf9eafd2996d1b46a84aa
Philipp Bende <bende@isp.uni-luebeck.de>
2024-04-24 13:59:21 +0200

    typo fix

--- src/test/java/ups/model/ExampleTest.java
+++ src/test/java/ups/model/ExampleTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Testing the Example class with its arbitrary arithmetical computations
  */
-public class ExampleTest {
+public class ExampleTest {
     /**
      * This tes case doesn't make any sense. It is just here as an example.
      */
